d2be5ce587b3c40d7e9e889cff7891d7ad97b290
Danyil <shelofast.daniil@gmail.com>
2026-02-02 16:47:35 +0200

    homework

--- src/main/kotlin/mate/academy/Main.kt
+++ src/main/kotlin/mate/academy/Main.kt
@@ -1,7 +1,16 @@
 package mate.academy
 
 fun greetUsers() {
-    // implement function
+    var i = 0
+    while (i < Int.MAX_VALUE) {
+        println("Please enter a name (or press Enter to quit):")
+        val name = readLine();
+        if (name?.isEmpty() == true) {
+            break
+        }
+        println("Hello, $name!")
+        i++
+    }
 }
 
 fun main() {
